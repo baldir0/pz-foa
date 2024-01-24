2bbcd3d3a83e50871e9b8625c784d3ed41db38f6
Kamil Lewucha <lewucha2000@gmail.com>
2024-01-24 21:27:40 +0100

    created order service + router

--- pz-foa-be/src/Services/order/orderService.ts
+++ pz-foa-be/src/Services/order/orderService.ts
@@ -1,7 +1,9 @@
-import { Response } from 'express';
 import { OrderEntity } from 'src/Entities/order.entity';
 import { UserEntity } from 'src/Entities/user.entity';
+import { OrderDataInterface } from 'src/Interfaces/order-interface';
+import { serviceResult } from 'src/Interfaces/serviceReturn-interface';
 import { DB } from 'src/utils/database/database';
+import { AuthErrorLackOfPrivilages } from 'src/utils/errors';
 import { Repository } from 'typeorm';
 
 class OrderService {
@@ -9,11 +11,70 @@ class OrderService {
     private orderRepo: Repository<OrderEntity> = DB.getRepository(OrderEntity)
   ) {}
 
-  public async getAll(res: Response) {}
-  public async get(orderId: string, res: Response) {}
-  public async update(orderData, res: Response) {}
-  public async delete(orderId: string, res: Response) {}
-  public async create(orderData, res: Response) {}
+  public async getAll(
+    user: UserEntity,
+    adminRequest?: boolean
+  ): Promise<serviceResult> {
+    const result = await this.orderRepo.find({
+      where: { userId: !adminRequest ? user.id : null },
+    });
+
+    return {
+      status: 200,
+      data: result,
+    };
+  }
+
+  public async get(user: UserEntity, orderId: string): Promise<serviceResult> {
+    const result = await this.orderRepo.findOne({
+      where: { id: orderId, userId: user.isAdmin ? null : user.id },
+    });
+
+    return {
+      status: 200,
+      data: result,
+    };
+  }
+
+  public async update(
+    user: UserEntity,
+    orderId: string,
+    orderData: OrderDataInterface
+  ): Promise<serviceResult> {
+    const result = await this.orderRepo.update(
+      {
+        id: orderId,
+        userId: user.isAdmin ? null : user.id,
+      },
+      orderData
+    );
+
+    return {
+      status: 200,
+      data: result,
+    };
+  }
+
+  public async delete(
+    user: UserEntity,
+    orderId: string
+  ): Promise<serviceResult> {
+    const result = await this.orderRepo.delete({ id: orderId });
+
+    return {
+      status: 200,
+      data: result,
+    };
+  }
+
+  public async create(user: UserEntity): Promise<serviceResult> {
+    const result = await this.orderRepo.insert({ userId: user.id });
+
+    return {
+      status: 201,
+      data: result,
+    };
+  }
 }
 
 export const orderService = new OrderService();
--- pz-foa-be/src/rotuers/order.router.ts
+++ pz-foa-be/src/rotuers/order.router.ts
@@ -1,50 +1,87 @@
 import { Router } from 'express';
+import { UserEntity } from 'src/Entities/user.entity';
+import { serviceResult } from 'src/Interfaces/serviceReturn-interface';
+import { authService } from 'src/Services/auth/authService';
+import { orderService } from 'src/Services/order/orderService';
+import { AuthErrorLackOfPrivilages } from 'src/utils/errors';
 
 const orderRouter = Router();
 
 orderRouter
-  .get('/list', (req, res, next) => {
+  .get('/list', async (req, res, next) => {
     try {
-      // TODO: Return list of user orders
-      // [ ]: User mush be logged in
+      const user: UserEntity = await authService.validate(req.cookies.jwt);
+
+      const result: serviceResult = await orderService.getAll(user);
+      res.status(result.status).json(result.data);
+      // [x]: Return list of user orders
+      // [x]: User mush be logged in
     } catch (err) {
       next(err);
     }
   })
-  .get('/list-all', (req, res, next) => {
+  .get('/list-all', async (req, res, next) => {
     try {
-      // TODO: Return list of all orders
-      // [ ]: Only users with admin privilages can access this endpoint
+      const user: UserEntity = await authService.validate(req.cookies.jwt);
+      if (!user.isAdmin) throw new AuthErrorLackOfPrivilages();
+
+      const result: serviceResult = await orderService.getAll(user, true);
+      res.status(result.status).json(result.data);
+      // [x]: Return list of all orders
+      // [x]: Only users with admin privilages can access this endpoint
     } catch (err) {
       next(err);
     }
   })
-  .get('/:id', (req, res, next) => {
+  .get('/:id', async (req, res, next) => {
     try {
-      // TODO: Get single order details
+      const user: UserEntity = await authService.validate(req.cookies.jwt);
+
+      const result: serviceResult = await orderService.get(user, req.params.id);
+      res.status(result.status).json(result.data);
+      // [X]: Get single order details
     } catch (err) {
       next(err);
     }
   })
-  .patch('/:id', (req, res, next) => {
+  .patch('/:id', async (req, res, next) => {
     try {
-      // TODO: Update single order
-      // [ ]: check is user the admin
-      // [ ]: check is user the owner
+      const user: UserEntity = await authService.validate(req.cookies.jwt);
+      const orderData = req.body;
+
+      const result: serviceResult = await orderService.update(
+        user,
+        req.params.id,
+        orderData
+      );
+      res.status(result.status).json(result.data);
+      // [x]: Update single order
+      // [x]: check is user the admin
+      // [x]: check is user the owner
     } catch (err) {
       next(err);
     }
   })
-  .delete('/:id', (req, res, next) => {
+  .delete('/:id', async (req, res, next) => {
     try {
-      // TODO: Remove single order
-      // [ ]: check if user is admin
+      const user: UserEntity = await authService.validate(req.cookies.jwt);
+      if (!user.isAdmin) throw new AuthErrorLackOfPrivilages();
+
+      const result: serviceResult = await orderService.delete(req.params.id);
+      res.status(result.status).send(result.data);
+      // [x]: Remove single order
+      // [x]: check if user is admin
     } catch (err) {
       next(err);
     }
   })
-  .put('', (req, res, next) => {
+  .put('/', async (req, res, next) => {
     try {
+      const user: UserEntity = await authService.validate(req.cookies.jwt);
+
+      const result: serviceResult = await orderService.create(user);
+      res.status(result.status).send(result.data);
+      // [x]: Create new Order
     } catch (err) {
       next(err);
     }
